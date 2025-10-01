de9b71e9ec3722472ff784f62b98510ef5db74ff
raharjo04 <diaz.raharjo04@gmail.com>
2025-10-01 09:43:36 +0700

    Penambahan Master Layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -5,6 +5,9 @@
 </head>
 
 <body>
+    @extends('master')
+    @section('title', 'Daftar Pegawai')
+    @section('content')
     <div class="container mt-5">
         <h1 class="mb-4">Daftar Pegawai</h1>
         <table border="1" cellpadding="5" cellspacing="0">
@@ -44,5 +47,6 @@
             </tbody>
         </table>
     </div>
+    @endsection
 </body>
 </html>
