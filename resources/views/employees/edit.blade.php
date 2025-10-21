209c7ef962837b0effb38ccaa5391ee0ed46e5fb
raharjo04 <diaz.raharjo04@gmail.com>
2025-10-21 17:50:48 +0700

    penambahan migrasi semua tabel

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Edit Data Pegawai</title>
 </head>
+
 <body>
     <h2>Edit Data Pegawai</h2>
     <form action="{{ route('employees.update', $employee->id) }}" method="POST">
@@ -10,35 +12,35 @@
         @method('PUT')
         <table>
             <tr>
-                <td>Nama Lengkap</td>   
-                <td><input type="text" name="nama_lengkap" value="{{ old('nama_lengkap', $employee->nama_lengkap) }}"></td>             
+                <td>Nama Lengkap</td>
+                <td><input type="text" name="nama_lengkap" value="{{ old('nama_lengkap', $employee->nama_lengkap) }}"></td>
             </tr>
             <tr>
-                <td>Email</td>   
-                <td><input type="email" name="email" value="{{ old('email', $employee->email) }}"></td>             
+                <td>Email</td>
+                <td><input type="email" name="email" value="{{ old('email', $employee->email) }}"></td>
             </tr>
             <tr>
-                <td>Nomor Telepon</td>   
-                <td><input type="text" name="nomor_telepon" value="{{ old('nomor_telepon', $employee->nomor_telepon) }}"></td>             
+                <td>Nomor Telepon</td>
+                <td><input type="text" name="nomor_telepon" value="{{ old('nomor_telepon', $employee->nomor_telepon) }}"></td>
             </tr>
             <tr>
-                <td>Tanggal Lahir</td>   
-                <td><input type="date" name="tanggal_lahir" value="{{ old('tanggal_lahir', $employee->tanggal_lahir) }}"></td>             
+                <td>Tanggal Lahir</td>
+                <td><input type="date" name="tanggal_lahir" value="{{ old('tanggal_lahir', $employee->tanggal_lahir) }}"></td>
             </tr>
             <tr>
-                <td>alamat</td>   
-                <td><input type="text" name="alamat" value="{{ old('alamat', $employee->alamat) }}"></td>             
+                <td>alamat</td>
+                <td><input type="text" name="alamat" value="{{ old('alamat', $employee->alamat) }}"></td>
             </tr>
             <tr>
-                <td>Tanggal Masuk</td>   
-                <td><input type="date" name="tanggal_masuk" value="{{ old('tanggal_masuk', $employee->tanggal_masuk) }}"></td>             
+                <td>Tanggal Masuk</td>
+                <td><input type="date" name="tanggal_masuk" value="{{ old('tanggal_masuk', $employee->tanggal_masuk) }}"></td>
             </tr>
             <tr>
                 <td>Status</td>
                 <td>
                     <select name="status">
                         <option value="aktif" {{ old('status', $employee->status) == 'aktif' ? 'selected' : '' }}>Aktif</option>
-                        <option value="tidak aktif" {{ old('status', $employee->status) == 'tidak aktif' ? 'selected' : '' }}>Tidak Aktif</option>
+                        <option value="nonaktif" {{ old('status', $employee->status) == 'nonaktif' ? 'selected' : '' }}>Tidak Aktif</option>
                     </select>
                 </td>
             </tr>
@@ -50,4 +52,5 @@
         </table>
     </form>
 </body>
+
 </html>
